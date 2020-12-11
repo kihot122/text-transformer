f6251e9bcf068185d791273691c3ec0562699a5d
Lewand118 <w.lewandowski118@wp.pl>
2020-12-11 11:40:52 +0100

    poprawa liczb na tekst, dodanie skrotow

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -14,21 +14,31 @@ public class TextTransformer {
 
         abbriviations.put("mgr", "magister");
         abbriviations.put("Mgr", "Magister");
+        abbriviations.put("MGR", "MAGISTER");
 
         abbriviations.put("dr", "doktor");
         abbriviations.put("Dr", "Doktor");
+        abbriviations.put("DR", "DOKTOR");
 
-        abbriviations.put("np", "na przykład");
-        abbriviations.put("Np", "Na przykład");
+        abbriviations.put("np.", "na przykład");
+        abbriviations.put("Np.", "Na przykład");
+        abbriviations.put("NP.", "NA PRZYKŁAD");
 
         abbriviations.put("prof.", "profesor");
         abbriviations.put("Prof.", "Profesor");
+        abbriviations.put("PROF..", "PROFESOR");
 
-        abbriviations.put("inż", "inżynier");
-        abbriviations.put("Inż", "Inżynier");
+        abbriviations.put("inż.", "inżynier");
+        abbriviations.put("Inż.", "Inżynier");
+        abbriviations.put("INŻ.", "INŻYNIER");
 
         abbriviations.put("hab.", "habilitowany");
         abbriviations.put("Hab.", "Habilitowany");
+        abbriviations.put("HAB.", "HABILITOWANY");
+
+        abbriviations.put("itp.", "i tym podobne");
+        abbriviations.put("Itp.", "I tym podobne");
+        abbriviations.put("ITP.", "I TYM PODOBNE");
 
         this.transforms = transforms;
     }
@@ -91,6 +101,8 @@ public class TextTransformer {
             String tmp = array[i];
             if(Character.isDigit(tmp.charAt(0))){
                 int number = Integer.parseInt(tmp);
+                if (number >= 10000)
+                    return text;
                 while(number > 0){
                     if(tmp.length() == 4){
                         if(tmp.charAt(0) == '1'){
