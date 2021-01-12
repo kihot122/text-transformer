a919a90512c7ea3639d5fc4564f59b87100aabe0
Lewand118 <w.lewandowski118@wp.pl>
2021-01-12 11:41:31 +0100

    tests?

--- src/main/java/pl/put/poznan/transformer/test/ToCapitalTest.java
+++ src/main/java/pl/put/poznan/transformer/test/ToCapitalTest.java
@@ -1,22 +1,18 @@
 package pl.put.poznan.transformer.test;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
 import pl.put.poznan.transformer.logic.InputString;
 import pl.put.poznan.transformer.logic.ToCapital;
 
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ShrinkAbbriviationTest {
+class ToCapitalTest {
 
     @Test
-    public void transform(){
-        String s = "magister";
+    public void transform() {
+        String s = "abc";
         InputString inputString = new InputString(s);
         ToCapital r = new ToCapital(inputString);
 
-        assertEquals("mgr", r.transform(), "Shrink");
+        assertEquals("ABC", r.transform(), "ToCapital");
     }
+
 }
--- src/main/java/pl/put/poznan/transformer/test/ToLowerTest.java
+++ src/main/java/pl/put/poznan/transformer/test/ToLowerTest.java
@@ -1,22 +1,20 @@
 package pl.put.poznan.transformer.test;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import pl.put.poznan.transformer.logic.InputString;
 import pl.put.poznan.transformer.logic.ToCapital;
 
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class ShrinkAbbriviationTest {
+class ToLowerTest {
 
     @Test
-    public void transform(){
-        String s = "magister";
+    public void transform() {
+        String s = "ABC";
         InputString inputString = new InputString(s);
         ToCapital r = new ToCapital(inputString);
 
-        assertEquals("mgr", r.transform(), "Shrink");
+        assertEquals("abc", r.transform(), "ToLower");
     }
+
 }
